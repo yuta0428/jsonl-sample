9dd479102fc1aa265e467fbbe636859fcb69c016
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2024-11-28 00:27:02 +0900

    Fix: build

--- sample-page/next.config.ts
+++ sample-page/next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "export", // 静的サイトエクスポートを有効化
-	distDir: "../docs", // 出力先を `docs/` に設定
-	basePath: "/jsonl-sample", // GitHub Pages用 (リポジトリ名に合わせて変更)
 };
 
 export default nextConfig;
