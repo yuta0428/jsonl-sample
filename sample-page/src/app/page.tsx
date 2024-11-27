f8ce3102434b58e1166dd0983ef10d397918e3a0
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2024-11-28 01:33:50 +0900

    Fix: globe.svg

--- sample-page/src/app/page.tsx
+++ sample-page/src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src={`${BASE_PATH}/globe.svg`}
             alt="Globe icon"
             width={16}
             height={16}
