8d2d957b08efae672dcd21708d805a4f521016f2
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2024-11-27 23:43:39 +0900

    Add: next.config

--- sample-page/next.config.ts
+++ sample-page/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	output: "export", // 静的サイトエクスポートを有効化
+	basePath: "/jsonl-sample", // GitHub Pages用 (リポジトリ名に合わせて変更)
 };
 
 export default nextConfig;
