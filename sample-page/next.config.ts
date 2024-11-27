c3412243b949187dd80590cedc93f8661d1b109a
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2024-11-27 23:48:41 +0900

    Add: distDir

--- sample-page/next.config.ts
+++ sample-page/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "export", // 静的サイトエクスポートを有効化
+	distDir: "docs", // 出力先を `docs/` に設定
 	basePath: "/jsonl-sample", // GitHub Pages用 (リポジトリ名に合わせて変更)
 };
 
